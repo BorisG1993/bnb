b7c1fde382529dceaeb2cdb9d15788192bf19d71
Boris Gurevich <borisgur1993@gmail.com>
2025-12-04 14:36:23 +0200

    info page template created

--- info.js
+++ info.js
@@ -1,23 +1,33 @@
-import { PageLoader, createFrame } from "./utils.js";
+import { PageLoader, makeFrame } from "./utils.js";
+const pageLoader = new PageLoader();
 pageLoader.contentPathPrefix = "content/info_content_";
 pageLoader.mediaPath = "content/info_images.json";
+let frameClassName = "infoFrame";
 
 function loadPage() {
-    pageLoader.loadContent(pageLoader.getContentPath(), renderContent);
-    pageLoader.loadContent(pageLoader.mediaPath, renderImages);
-}
-
-function renderContent(data) {
-
-}
-
-function renderImages(data) {
-
+    pageLoader.loadContent(pageLoader.getContentPath(), createFrames);
 }
 
 function changeLanguage(newLang) {
     pageLoader.setLanguage(newLang);
     pageLoader.loadContent(pageLoader.getContentPath(), renderContent);
+}
+
+async function createFrames() {
+    const response = await fetch(pageLoader.getContentPath());
+    const topics = await response.json();
+
+    const directions = ["left", "right"];
+
+    for (let i = 0; i < topics.length; i++) {
+        const topic = topics[i];
+        const direction = directions[i % directions.length];
+
+        topic.class = "info-frame";
+        topic.direction = direction
+
+        document.body.appendChild(makeFrame(frameClassName, topic));
+    }
 }
 
 window.changeLanguage = changeLanguage;
