600ae49624f65a22a454770816af3e6673779577
Boris Gurevich <borisgur1993@gmail.com>
2025-12-16 14:14:25 +0200

    first version of website created

---
+++ index.js
@@ -0,0 +1,55 @@
+import { PageLoader, activateImageOverlay } from "./utils.js";
+const pageLoader = new PageLoader();
+pageLoader.contentPathPrefix = "content/main_page_content_";
+pageLoader.mediaPath = "content/main_page_images.json";
+
+function loadPage() {
+    pageLoader.loadContent(pageLoader.getContentPath(), renderContent);
+    pageLoader.loadContent(pageLoader.mediaPath, renderImages);
+}
+
+function renderContent(data) {
+    document.getElementById("main-page-header-title").textContent = data.title;
+    document.getElementById("info").textContent = data.info;
+    document.getElementById("activities").textContent = data.activities;
+    document.getElementById("photos").textContent = data.photos;
+    
+    pageLoader.readText(data.summary_text_link, (text) => {
+        document.getElementById("main-page-summary").textContent = text;
+    });
+
+    document.getElementById("contactTitle").textContent = data.contact_title;
+    document.getElementById("contactAddress").textContent = data.contact.address;
+    document.getElementById("contactPhone1").textContent = data.contact.phone1;
+    document.getElementById("contactPhone2").textContent = data.contact.phone2;
+
+    }
+
+function renderImages(data) {
+    const imagesDiv = document.getElementById("images");
+    imagesDiv.innerHTML = "";
+    data.images.forEach(src => {
+        const img = document.createElement("img");
+        img.src = src;
+        activateImageOverlay(img);
+        imagesDiv.appendChild(img);
+    });
+}
+
+function changeLanguage(newLang) {
+    if (pageLoader.getLanguage() === newLang) return;
+    pageLoader.setLanguage(newLang);
+    pageLoader.loadContent(pageLoader.getContentPath(), renderContent);
+}
+
+function goTo(page) {
+    window.location.href = `${page}.html`;
+}
+
+window.changeLanguage = changeLanguage;
+window.goTo = goTo;
+
+document.addEventListener("DOMContentLoaded", loadPage);
+
+
+
