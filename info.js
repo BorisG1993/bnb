e88e748b1ce11d98d2fc9f17d75300fa03ccc4fc
Boris Gurevich <borisgur1993@gmail.com>
2025-12-25 18:06:19 +0200

    - Added location at arrival - Fixed edge case logic with missing liks or images in json.

--- info.js
+++ info.js
@@ -1,12 +1,12 @@
 import { PageLoader, FrameCreator } from "./utils.js";
 const frameClassName = "info-frame";
 const infoImagesPath = "assets/images/info/info_images.json";
+const infoLinksPath = "assets/links/info/info_links.json";
 
 const pageLoader = new PageLoader();
 pageLoader.contentPathPrefix = "content/info_content_";
 
-const frameCreator = new FrameCreator(pageLoader, frameClassName, infoImagesPath);
-
+const frameCreator = new FrameCreator(pageLoader, frameClassName, infoImagesPath, infoLinksPath);
 
 function loadPage() {
     pageLoader.loadContent(pageLoader.getContentPath(), frameCreator.createFrames);
